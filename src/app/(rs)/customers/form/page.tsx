46b286383621cd63dac8995461a096e4373fb4de
Popoola Abdulhafeez <hafeezolayinka2005@gmail.com>
2025-03-17 15:01:58 +0100

    Add customer form page and database query for customer retrieval

---
+++ src/app/(rs)/customers/form/page.tsx
@@ -0,0 +1,34 @@
+import { getCustomer } from '@/lib/queries/getCustomer';
+
+export default async function CustomerFormPage({
+	searchParams,
+}: {
+	searchParams: Promise<{ [key: string]: string | undefined }>;
+}) {
+	try {
+		const { customerId } = await searchParams;
+
+		// Edit customer form
+		if (customerId) {
+			const customer = await getCustomer(parseInt(customerId));
+
+			if (!customer) {
+				return (
+					<>
+						<h2 className="text-2xl mb-2">
+							Customer ID #{customerId} not found
+						</h2>
+					</>
+				);
+			}
+
+            // put customer form component here
+		} else {
+            // New customer form component here
+        }
+	} catch (error) {
+		if (error instanceof Error) {
+			throw error;
+		}
+	}
+}
